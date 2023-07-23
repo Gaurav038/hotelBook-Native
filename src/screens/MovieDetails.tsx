17a93e507a114d2fb682e5111f7be62053f0f0c4
GauravSingh <gauravkarki038@gmail.com>
2023-07-23 20:33:59 +0530

    home screen added

---
+++ src/screens/MovieDetails.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>MovieDetails</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default MovieDetails
---
+++ src/screens/UserAccount.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UserAccount = () => {
+  return (
+    <View>
+      <Text>UserAccount</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default UserAccount
